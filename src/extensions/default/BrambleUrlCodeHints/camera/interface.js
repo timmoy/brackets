3109eb288820800b3e4a30312e4c657b829a3a6b
timmoy <tmoy@live.ca>
2017-03-16 03:39:25 -0400

    initial implementation of caman for single filter; saving not working for filters

--- src/extensions/default/BrambleUrlCodeHints/camera/interface.js
+++ src/extensions/default/BrambleUrlCodeHints/camera/interface.js
@@ -89,9 +89,34 @@ define(function (require, exports, module) {
                 return self.camera.fail();
             }
 
-            var binaryDataStr = /^data:image\/png;base64,(.+)/.exec(data)[1];
+            var binaryDataStr;
+            if(img.src){
+              console.log("copy of image is here");
+              Caman(img, function () {
+                this.brightness(5);
+                this.render(function () {
+                  this.save(img.src+".png");
+                });
+              });
+              binaryDataStr = /^data:image\/png;base64,(.+)/.exec(img.src+".png")[1];
+              console.log("alternate saved");
+            }else{
+              binaryDataStr = /^data:image\/png;base64,(.+)/.exec(data)[1];
+            }
+
             self.camera.savePhoto(base64ToBuffer(binaryDataStr));
         }
+
+        /*function updateFilter(){
+        +      var select = document.getElementById("filters-select");
+        +      if(select.options[select.selectedIndex].value != "none"){
+        +        //alert(select.options[select.selectedIndex].value + " filter applied");
+        +        document.getElementById("selfie-photo").className = "polaroid " + select.options[select.selectedIndex].value;
+        +      }else{
+        +        document.getElementById("selfie-photo").className = "polaroid";
+        +      }
+        +    }*/
+
 
         var context = this.canvas.interface.getContext("2d");
         this.canvas.interface.width = _width;
@@ -100,6 +125,60 @@ define(function (require, exports, module) {
 
         // Update the photo component with the snapped photo
         this.photo.update();
+
+        //pattern from http://tutorialzine.com/2013/02/instagram-filter-app/
+        //$(function() {
+
+          //var Caman = require('caman').Caman;
+          var canvas = document.getElementById("canvas");
+          var ctx = canvas.getContext('2d');
+
+          /* Enable Cross Origin Image Editing */
+          var img = new Image();
+          img.crossOrigin = '';
+          img.src = self.photo.data;
+          console.log("img width: "+ img.width + " height " + img.height + "source: " + img.src);
+
+          img.onload = function() {
+            canvas.width = img.width;
+            canvas.height = img.height;
+            console.log("loaded width: "+ img.width + " height " + img.height);
+            ctx.drawImage(img, 0, 0, img.width, img.height);
+          }
+
+          var $reset = $('#resetbtn');
+          var $pinhole = $('#pinholebtn');
+          $reset.on('click', function(e) {
+            $('input[type=range]').val(0);
+            Caman('#canvas', img, function() {
+              this.revert(false);
+              this.render();
+              console.log("reset");
+            });
+          });
+
+          /* In built filters */
+          $pinhole.on('click', function(e) {
+            Caman('#canvas', img, function() {
+              this.pinhole().render();
+              console.log("filtered");
+            });
+          });
+
+          // Listen for clicks on the filters
+
+          /*filters.click(function(e){
+            console.log("clicked");
+
+            Caman("#selfie-canvas", function () {
+              this.invert();
+              console.log("inverted");
+              this.render();
+              console.log("rendered");
+            });
+          });//end of click*/
+        //});
+        //end of pattern
 
         this.saveButton.removeEventListener("click", persistPhoto);
         this.saveButton.addEventListener("click", persistPhoto);
