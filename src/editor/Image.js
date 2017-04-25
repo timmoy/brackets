fa07edef892a8b8118143422f47c30277c919d42
timmoy <tmoy@live.ca>
2017-04-25 17:02:24 -0400

    fixed a variable name typo [FileUtils] to [FilerUtils]

--- src/editor/Image.js
+++ src/editor/Image.js
@@ -3,7 +3,7 @@ define(function (require, exports, module) {
 
     var Caman           = require("caman");
     var FilerFileSystem = require("fileSystemImpl");
-    var FileUtils       = require("filesystem/impls/filer/FilerUtils");
+    var FilerUtils       = require("filesystem/impls/filer/FilerUtils");
     var FileSystemCache = require("filesystem/impls/filer/FileSystemCache");
     var Path            = require("filesystem/impls/filer/BracketsFiler").Path;
     var mimeFromExt     = require("filesystem/impls/filer/lib/content").mimeFromExt;
@@ -22,7 +22,7 @@ define(function (require, exports, module) {
 
         $saveBtn.click(function() {
             var imageBase64Data = image.canvas.toDataURL(imageMimeType);
-            var data = FileUtils.base64ToBuffer(imageDataRegex.exec(imageBase64Data)[1]);
+            var data = FilerUtils.base64ToBuffer(imageDataRegex.exec(imageBase64Data)[1]);
 
             FilerFileSystem.writeFile(imagePath, data, {encoding: null}, function(err) {
                 if(err) {
